db2bc041c0dc090b8fa607d0657ac5c52e2f73d3
Vikram <83545008+Vikram-amin@users.noreply.github.com>
2022-06-10 17:19:43 +0530

    toggle switch is done

---
+++ src/Components/Switch.jsx
@@ -0,0 +1,16 @@
+
+import React from "react"
+import './style.css'
+
+const Switch = () => {
+return (
+  <>
+  <label className="switch">
+    <input type="checkbox"/>
+    <span className="slider"></span>
+  </label>
+  </>
+)
+}
+
+export default Switch
